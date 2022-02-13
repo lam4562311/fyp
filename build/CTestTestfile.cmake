8ba7233c5e5d07d8e08b62cdb6576c2f8db3235f
lam4562311 <lam4562311@gmail.com>
2022-02-13 20:39:14 +0800

    first commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/ubuntu/car_base-master/src
+# Build directory: /home/ubuntu/car_base-master/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("ps4-bot")
